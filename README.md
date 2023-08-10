0b1774b117c520490f9a5fc4c3e76039d660f549
Pranoy Kundu <pranoy1998k@gmail.com>
2023-08-11 00:20:31 +0530

    create snapshot and create a pvc out of the same snapshot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # k8s-volume-snapshot-operator
 
+## Usage 
+
+```bash
+go run main.go -kubeconfig=<path-to-kube-config> -pvc=<existing-pvc-name> -snapshot=<snapcot-name>
+```
--- main.go
+++ main.go
@@ -6,26 +6,74 @@ import (
 	"fmt"
 	"log"
 
+	snapshotsv1 "github.com/kubernetes-csi/external-snapshotter/client/v4/apis/volumesnapshot/v1"
+	snapshotclientset "github.com/kubernetes-csi/external-snapshotter/client/v4/clientset/versioned"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	config := flag.String("kubeconfig", "/home/pranoy/.kube/config", "Path to a kubeconfig.")
+	kubeconfig := flag.String("kubeconfig", "/path/to/kubeconfig", "location of your kubeconfig file")
+	pvcName := flag.String("pvc", "", "name of the PVC")
+	snapshotName := flag.String("snapshot", "", "name of the snapshot")
 	flag.Parse()
-	client, err := clientcmd.BuildConfigFromFlags("", *config)
-	if err != nil {
-		log.Fatalf("error while building config", err.Error())
-	}
-	clientset, err := kubernetes.NewForConfig(client)
 
-	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
+	fmt.Printf("Creating snapshot %s for PVC %s\n", *snapshotName, *pvcName)
+	config, _ := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	snapshotClient, _ := snapshotclientset.NewForConfig(config)
+
+	// Create a snapshot
+	snapshot := &snapshotsv1.VolumeSnapshot{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      *snapshotName,
+			Namespace: "default",
+		},
+		Spec: snapshotsv1.VolumeSnapshotSpec{
+			Source: snapshotsv1.VolumeSnapshotSource{
+				PersistentVolumeClaimName: pvcName,
+			},
+		},
+	}
+
+	volumeSnapshot, err := snapshotClient.SnapshotV1().VolumeSnapshots("default").Create(context.TODO(), snapshot, metav1.CreateOptions{})
 	if err != nil {
-		fmt.Println("error while listing pods", err.Error())
-		return
+		log.Fatalf("Error creating snapshot: %v", err)
 	}
-	for _, pod := range pods.Items {
-		fmt.Println(pod.Name)
+	fmt.Printf("Created snapshot: %s\n", volumeSnapshot.Name)
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Error creating clientset: %v", err)
 	}
+	createPVCFromSnapshot(clientset, "pvc-from-snapshot", "manual-snapshot")
+}
+
+func createPVCFromSnapshot(clientset *kubernetes.Clientset, pvcName, storageClassName string) error {
+	// Create a PVC from the snapshot
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pvcName,
+			Namespace: "default",
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			StorageClassName: &storageClassName,
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse("1Gi"),
+				},
+			},
+			AccessModes: []corev1.PersistentVolumeAccessMode{
+				corev1.ReadWriteOnce,
+			},
+		},
+	}
+
+	pvc, err := clientset.CoreV1().PersistentVolumeClaims("default").Create(context.TODO(), pvc, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("Error creating PVC: %v", err)
+	}
+	fmt.Printf("Created PVC: %s\n", pvc.Name)
+	return nil
 }
